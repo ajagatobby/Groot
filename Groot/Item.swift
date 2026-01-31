1970fd2da8763b31f211af0583d68352d4e5e3f1
ajagatobby <150749949+ajagatobby@users.noreply.github.com>
2026-01-31 05:55:55 +0100

    Initial Commit

---
+++ Groot/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Groot
+//
+//  Created by Abdulbasit Ajaga on 31/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
